86d964dd06928a93b01fd0849f245c532fb9cfd0
MorisZheng <s04044mort@gmail.com>
2023-04-12 16:50:46 +0800

    1

--- main.cpp
+++ main.cpp
@@ -5,9 +5,16 @@ PwmOut pwm(D6);
 // main() runs in its own thread in the OS
 int main()
 {
+    float sample[10];
+    sample[0] = 0.1;
+    
+    int cnt = 0;
     pwm.period_us(50);
     while (true) {
-
+        for (int i = 0; i < 9; i++){
+            pwm.write(0.1 + float(i) / 10);
+            ThisThread::sleep_for(10ms);
+        }
     }
 }
 
